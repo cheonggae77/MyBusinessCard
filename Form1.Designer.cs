5611e2772ac0687a47e4408c67161b493e9cd12f
SANGHUN CHOI <cheonggae77@gmail.com>
2026-03-06 13:10:48 +0900

    라벨과 버튼 컨트롤 추가 및 텍스트 수

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,34 +33,35 @@
             label2 = new Label();
             pictureBox1 = new PictureBox();
             label3 = new Label();
-            label4 = new Label();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("맑은 고딕", 40F);
-            label1.Location = new Point(545, 48);
+            label1.Font = new Font("맑은 고딕", 50F);
+            label1.Location = new Point(381, 60);
             label1.Name = "label1";
-            label1.Size = new Size(239, 89);
+            label1.Size = new Size(299, 112);
             label1.TabIndex = 1;
             label1.Text = "최상훈";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("맑은 고딕", 15F);
-            label2.Location = new Point(385, 172);
+            label2.Font = new Font("맑은 고딕", 25F);
+            label2.ForeColor = Color.RoyalBlue;
+            label2.Location = new Point(381, 216);
             label2.Name = "label2";
-            label2.Size = new Size(609, 35);
+            label2.Size = new Size(425, 57);
             label2.TabIndex = 2;
-            label2.Text = "수원대학교 지능형SW융합대학 컴퓨터학부 컴퓨터sw";
+            label2.Text = "컴퓨터학부 컴퓨터sw";
             // 
             // pictureBox1
             // 
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(51, 60);
+            pictureBox1.Location = new Point(69, 60);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(245, 315);
             pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
@@ -70,29 +71,34 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("맑은 고딕", 15F);
-            label3.Location = new Point(385, 239);
+            label3.Font = new Font("맑은 고딕", 23F);
+            label3.ForeColor = Color.ForestGreen;
+            label3.Location = new Point(381, 273);
             label3.Name = "label3";
-            label3.Size = new Size(609, 35);
+            label3.Size = new Size(502, 52);
             label3.TabIndex = 4;
-            label3.Text = "수원대학교 지능형SW융합대학 컴퓨터학부 컴퓨터sw";
+            label3.Text = "cheonggae77@suwon.ac.kr";
             // 
-            // label4
+            // button1
             // 
-            label4.AutoSize = true;
-            label4.Font = new Font("맑은 고딕", 15F);
-            label4.Location = new Point(385, 302);
-            label4.Name = "label4";
-            label4.Size = new Size(609, 35);
-            label4.TabIndex = 5;
-            label4.Text = "수원대학교 지능형SW융합대학 컴퓨터학부 컴퓨터sw";
+            button1.BackColor = Color.Transparent;
+            button1.FlatAppearance.BorderColor = Color.FromArgb(255, 128, 0);
+            button1.FlatAppearance.BorderSize = 2;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("맑은 고딕", 15F);
+            button1.Location = new Point(469, 395);
+            button1.Name = "button1";
+            button1.Size = new Size(251, 58);
+            button1.TabIndex = 5;
+            button1.Text = "배경색 랜덤 변경";
+            button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(9F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1053, 521);
-            Controls.Add(label4);
+            ClientSize = new Size(972, 503);
+            Controls.Add(button1);
             Controls.Add(label3);
             Controls.Add(pictureBox1);
             Controls.Add(label2);
@@ -109,6 +115,6 @@
         private Label label2;
         private PictureBox pictureBox1;
         private Label label3;
-        private Label label4;
+        private Button button1;
     }
 }
